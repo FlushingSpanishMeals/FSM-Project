d97aa4bd1a97fb8fec869db607711320e120f4a6
sahn54 <s.ahn7@icloud.com>
2022-10-21 21:56:20 -0400

    update-s

--- js/index.js
+++ js/index.js
@@ -12,3 +12,14 @@ function removeActiveClasses(){
         panel.classList.remove('active')
     })
 }
+
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav(){
+    if(window.scrollY > nav.offsetHeight +150){
+        nav.classList.add('active')
+    } else{
+        nav.classList.remove('active')
+    }
+}
